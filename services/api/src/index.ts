38cfbc5d6723976eb87462a45e80fc37ed7865ae
joanne5548 <joanne.kim0328@gmail.com>
2025-08-30 12:30:03 -0500

    watch ts files and setup server

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,5 +1,24 @@
-import express from 'express';
+import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
+import dotenv from "dotenv";
 
-express;
+dotenv.config();
 
-console.log("hello!");
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+// TODO: Set up kafkajs and work on post request
+// app.post("/", async (req: Request, res: Response) => {
+
+// });
+
+app.get("/", async (req: Request, res: Response) => {
+    console.log("yoyoyoyoyoyoyo yo!");
+});
+
+const domain = process.env.SERVER_DOMAIN;
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`[server]: API Server is running at http://${domain}:${port}`);
+});
