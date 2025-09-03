65f9241d39ea27452d6c3e1d907cf054aee25404
joanne5548 <joanne.kim0328@gmail.com>
2025-09-02 21:52:53 -0500

    pet actions consumer running

--- services/event-processor/src/index.ts
+++ services/event-processor/src/index.ts
@@ -1,4 +1,4 @@
-import { Kafka, type Consumer } from 'kafkajs';
+import { Kafka, type Consumer, type Producer } from 'kafkajs';
 
 const kafka: Kafka = new Kafka({
     clientId: "event-processor",
@@ -6,17 +6,28 @@ const kafka: Kafka = new Kafka({
 });
 
 const consumer: Consumer = kafka.consumer({ groupId: "actions-consumer" });
+// const producer: Producer = kafka.producer();
 
 // TODO: add better startup error handling for connect & subscribe
 consumer.connect().catch(console.error);
-consumer.subscribe({ topic: "pet-actions", fromBeginning: true});
+consumer.subscribe({ topic: "pet-actions", fromBeginning: true}).catch(console.error);
 
-// TODO: fill in switch case statements
+// producer.connect().catch(console.error);
+
 consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-        switch (message.value) {
+        // console.log({
+        //     key: message.key.toString(),
+        //     value: message.value.toString(),
+        //     headers: message.headers,
+        // })
+
+        // if key exists, proceed
+        
+        switch (message.value.toString()) {
             case "feed":
                 // do feed operation
+                console.log("you feeded the panda!");
                 break
             case "play":
                 // play operation
